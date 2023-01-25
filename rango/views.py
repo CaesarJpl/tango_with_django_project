44013c116c78ada99596c2ac4ae9e3a28f4ab077
“PEILIN <2575240j@student.gla.ac.uk>
2023-01-25 14:35:05 +0000

    first commit

--- rango/views.py
+++ rango/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+	context_dic = {'boldmessage' : 'Crunchy, creamy, cookie, candy, cupcake!'}
+	return render(request, 'rango/index.html', context= context_dic)
+	
 
 	
 def about(request):
-	return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+	context_dic = {'boldmessage' : 'This tutorial has been put together by PEILIN!'}
+	return render(request, 'rango/about.html', context= context_dic) 
