6a1b457a1a9c5dac90101579629e3fc26beb0881
“PEILIN <2575240j@student.gla.ac.uk>
2023-02-07 13:57:06 +0000

    chapter 8

--- rango/views.py
+++ rango/views.py
@@ -24,8 +24,9 @@ def index(request):
 
 	
 def about(request):
-	context_dic = {'boldmessage' : 'This tutorial has been put together by PEILIN!'}
-	return render(request, 'rango/about.html', context= context_dic) 
+	print(request.method)
+	print(request.user)
+	return render(request, 'rango/about.html', {})
 
 
 
